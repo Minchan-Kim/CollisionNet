fc1382dcd6a6887ff35c7c56d77722a85fdf682a
Minchan-Kim <k21mchan@gmail.com>
2020-07-26 10:33:37 +0900

    log file generation

--- analysis.py
+++ analysis.py
@@ -52,7 +52,8 @@ class FalseNegatives(tf.keras.metrics.Metric):
 parser = argparse.ArgumentParser()
 parser.add_argument('name', nargs = '+')
 parser.add_argument('--time_window', type = int, default = 32)
-parser.add_argument('--timesteps', type = int, default = 2000)
+parser.add_argument('--timesteps', type = int, default = 20000)
+parser.add_argument('--dataset', type = str, default = 'test')
 parser.add_argument('--dtype', type = str, default = '')
 parser.add_argument('--tool', type = str, default = '')
 args = parser.parse_args()
@@ -60,6 +61,7 @@ args = parser.parse_args()
 name = args.name[0]
 time_window = args.time_window
 timesteps = args.timesteps
+dataset_type = args.dataset
 dtype = args.dtype
 tool = args.tool
 
@@ -80,6 +82,8 @@ model.compile(
         FalseNegatives(),
         ])
 
+log_file = open('/home/dyros/mc_ws/CollisionNet/analysis/' + name + '_on_' + dataset_type + '.txt', 'w')
+
 if (dtype != '') and (tool != ''):
     pattern = '*' + dtype + '_' + tool + '.tfrecord'
 elif dtype != '':
@@ -89,18 +93,28 @@ elif tool != '':
 else:
     pattern = '*.tfrecord'
 
-directory = '/home/dyros/mc_ws/CollisionNet/data/' + str(time_window) + '/test'
+directory = '/home/dyros/mc_ws/CollisionNet/data/' + str(time_window) + '/' + dataset_type
 Paths = Path(directory).glob(pattern)
 paths = [str(path) for path in Paths]
-dataset = ds.OrderedDataset(paths, 49, time_window, 100, processed = False)
+if dataset_type == 'training':
+    processed = True
+else:
+    processed = False
+dataset = ds.OrderedDataset(paths, 49, time_window, 100, processed = processed)
 result = model.evaluate(dataset, verbose = 0)
 predictions = model.predict(dataset)
 
 false_positives = int(result[2])
 false_negatives = int(result[3])
-print('Accuracy: {:.2}'.format(result[1]))
-print('False positives: {}'.format(false_positives))
-print('False negatives: {}'.format(false_negatives))
+line = 'Accuracy: {:.2}'.format(result[1])
+print(line)
+log_file.write(line + '\n')
+line = 'False positives: {}'.format(false_positives)
+print(line)
+log_file.write(line + '\n')
+line = 'False negatives: {}'.format(false_negatives)
+print(line)
+log_file.write(line + '\n')
 
 paths = [(path[:-8] + 'csv') for path in paths]
 dataframes = [pd.read_csv(path, header = None, skiprows = 31) for path in paths]
@@ -110,10 +124,14 @@ labels = dataframe.to_numpy()
 length = (labels.shape)[0]
 y = labels[:, 0]
 positives = np.sum(y)
-print("False negative rate: {:.3}".format(false_negatives / float(positives)))
+line = 'False negative rate: {:.3}'.format(false_negatives / float(positives))
+print(line)
+log_file.write(line + '\n')
 
 JTS = labels[:, 2]
 DOB = labels[:, 3]
+
+log_file.close()
 
 start = 0
 for i in range(length):
@@ -133,5 +151,5 @@ plt.ylabel('Collision Probability')
 plt.legend()
 plt.xlim(start, (stop - 1))
 plt.ylim(0, 1.1)
-plt.savefig('/home/dyros/mc_ws/CollisionNet/analysis/' + name + '.png')
+plt.savefig('/home/dyros/mc_ws/CollisionNet/analysis/' + name + '_on_' + dataset_type + '.png')
 plt.clf()
